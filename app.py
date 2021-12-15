392b477938c9b0d9180198fb7b44b842d5605466
vipin indoria <vipinindoria@gmail.com>
2021-12-16 00:22:40 +0530

    Added basic dictionary implimentation and graph implementatin

--- app.py
+++ app.py
@@ -1,4 +1,93 @@
 import argparse
+from itertools import product
+
+
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def hash(self, key):
+        return sum(ord(c) for c in key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash(key)
+        if self.table[index] is not None:
+            for kvp in self.table[index]:
+                if kvp[0] == key:
+                    kvp[1] = value
+                    break
+            else:
+                self.table[index].append([key, value])
+        else:
+            self.table[index].append((key, value))
+
+    def get(self, key):
+        index = self.hash(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return v
+        return None
+
+    def delete(self, key):
+        index = self.hash(key)
+        for i, (k, v) in enumerate(self.table[index]):
+            if k == key:
+                del self.table[index][i]
+                return
+        raise KeyError(key)
+
+    def __str__(self):
+        return str(self.table)
+
+    def __repr__(self):
+        return str(self)
+
+    def __len__(self):
+        return len(self.table)
+
+    def __iter__(self):
+        for i in range(len(self.table)):
+            for k, v in self.table[i]:
+                yield k, v
+
+    def __contains__(self, key):
+        index = self.hash(key)
+        for k, v in self.table[index]:
+            if k == key:
+                return True
+        return False
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        self.insert(key, value)
+
+    def __delitem__(self, key):
+        self.delete(key)
+
+
+class GraphUtils:
+    def __init__(self, vertices):
+        self.buckets = HashTable(10)
+        self.graph = HashTable(10)
+        self.vertices = vertices
+
+    def build_graph(self):
+        for vertex in self.vertices:
+            for i in range(len(vertex)):
+                bucket = '{}_{}'.format(vertex[:i], vertex[i + 1:])
+                self.buckets.insert(bucket, vertex)
+
+        print(self.buckets.table)
+        for bucket, mutual_neighbors in self.buckets.table:
+            for vertex1, vertex2 in product(mutual_neighbors, repeat=2):
+                if vertex1 != vertex2:
+                    self.graph.insert(vertex1, vertex2)
+                    self.graph.insert(vertex2, vertex1)
+
+        return self.graph
 
 
 class ShortestTransformationSequence:
@@ -10,6 +99,9 @@ class ShortestTransformationSequence:
     def solve(self):
         if self.s == self.t:
             return 0, None
+
+        graph = GraphUtils(self.words).build_graph()
+        print(graph)
         return None, None
 
 
